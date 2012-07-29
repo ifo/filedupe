4b09cb54584465d49c4d387e83b5a247f141b73c
ifo <raygunomix@gmail.com>
2012-07-29 16:06:36 -0400

    changed command to 'find temp -type d' which gets all directories recursively and prints them out, one per line

--- folderdupe.lua
+++ folderdupe.lua
@@ -1,9 +1,4 @@
-local file = io.popen('ls -d */','r')
---local file = io.popen('temp/ls -al','r')
+local file = io.popen('find temp -type d')
 local toprint = file:read('*all')
 file:close()
 print(toprint)
-
-local file2 = io.popen('/bin/ls -d */')
-local toprint2 = file2:read('*all')
-print(toprint2)
