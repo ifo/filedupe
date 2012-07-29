635e0a21b5fce186afcf31789dcb01e5472eb81e
ifo <raygunomix@gmail.com>
2012-07-29 16:29:58 -0400

    all folders are now recorded and written line by line into folders.txt

--- folderdupe.lua
+++ folderdupe.lua
@@ -1,4 +1,14 @@
-local file = io.popen('find temp -type d')
-local toprint = file:read('*all')
-file:close()
-print(toprint)
+local input = io.popen('find temp -type d')
+local output = io.open('temp/folders.txt', 'w+')
+
+local file = ""
+
+while true do
+  local line = input:read('*line')
+  if line == nil then break end
+  file = file .. line .. "\n"
+end
+--local toprint = file:read('*all')
+--file:close()
+--print(toprint)
+output:write(file)
