c1e63dc910e603bd1abb2b5ee09a2dcda9cb9a83
ifo <raygunomix@gmail.com>
2012-07-29 16:03:33 -0400

    trying subdirectories (not working)

--- folderdupe.lua
+++ folderdupe.lua
@@ -1,4 +1,9 @@
 local file = io.popen('ls -d */','r')
+--local file = io.popen('temp/ls -al','r')
 local toprint = file:read('*all')
 file:close()
 print(toprint)
+
+local file2 = io.popen('/bin/ls -d */')
+local toprint2 = file2:read('*all')
+print(toprint2)
