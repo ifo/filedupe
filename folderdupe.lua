4755557c4cc8e8e08323c5f28c2b817689e5457c
ifo <raygunomix@gmail.com>
2012-07-29 16:46:13 -0400

    basic regex working to capture folder name and path

--- folderdupe.lua
+++ folderdupe.lua
@@ -3,12 +3,13 @@ local output = io.open('temp/folders.txt', 'w+')
 
 local file = ""
 
+local foldertable = {}
+
 while true do
   local line = input:read('*line')
   if line == nil then break end
   file = file .. line .. "\n"
+  local path, folder = line:match('(.*/)(.-)$')
+  print(path, folder)
 end
---local toprint = file:read('*all')
---file:close()
---print(toprint)
 output:write(file)
