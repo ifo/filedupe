c75c18788a2b980e7fc6a61ea8ee62f152c04f5a
ifo <raygunomix@gmail.com>
2012-07-29 17:20:20 -0400

    output table to tab deliminated text file, one line per unique folder name

--- folderdupe.lua
+++ folderdupe.lua
@@ -8,7 +8,6 @@ local foldertable = {}
 while true do
   local line = input:read('*line')
   if line == nil then break end
-  file = file .. line .. "\n"
   local path, folder = line:match('(.*/)(.-)$')
   --print(path, folder)
   if folder ~= nil then
@@ -21,8 +20,9 @@ while true do
     --print(folder, foldertable.folder[1])
   end
 end
-output:write(file)
 
 for key,value in pairs(foldertable) do
-  print (key,table.concat(value,','))
+  file = file .. key .. '\t' .. table.concat(value,'\t') .. '\n'
 end
+
+output:write(file)
