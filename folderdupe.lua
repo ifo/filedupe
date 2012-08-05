0a3b678361f3a05193bbf653c1947a090354b7b9
Steve McCarthy <raygunomix@gmail.com>
2012-08-05 14:10:08 -0400

    failed attempt at command change to add -ls to find (always returns same folder size regardless)

--- folderdupe.lua
+++ folderdupe.lua
@@ -1,7 +1,10 @@
 --local input = io.popen('find temp -type d')
 -- above command is being changed to 'find temp -type d -ls'
 -- to give folder sizes for comparison
+-- this requires additional line processing (2 per folder)
+
 local input = io.open('inputfolders.txt')
+
 --local output = io.open('temp/folders.txt', 'w+')
 local output = io.open('folders.txt', 'w+')
 
@@ -12,6 +15,7 @@ local foldertable = {}
 while true do
   local line = input:read('*line')
   if line == nil then break end
+
   local path, folder = line:match('(.*/)(.-)$')
   --print(path, folder)
   if folder ~= nil then
