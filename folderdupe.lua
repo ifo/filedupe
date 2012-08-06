5840a85edb42735d179d76f82b0228b792269014
Steve McCarthy <raygunomix@gmail.com>
2012-08-05 23:52:22 -0400

    code cleanup

--- folderdupe.lua
+++ folderdupe.lua
@@ -1,43 +1,9 @@
--- this is now running lua 5.1, and will be using lua file system
 require('lfs')
 
 --[[ for time testing (also uncomment bottom)
 local x = os.clock()
 for i = 1,100000 do
 --]]
-
---[[ old code, use for refenence (data structure)
-
-local input = io.open('inputfolders.txt')
-local output = io.open('folders.txt', 'w+')
-local file = ""
-local foldertable = {}
-
-while true do
-  local line = input:read('*line')
-  if line == nil then break end
-
-  local path, folder = line:match('(.*/)(.-)$')
-  --print(path, folder)
-  if folder ~= nil then
-    if foldertable[folder] == nil then
-      foldertable[folder] = {}
-      table.insert(foldertable[folder],path)
-    else
-      table.insert(foldertable[folder],path)
-    end
-    --print(folder, foldertable.folder[1])
-  end
-end
-
-for key,value in pairs(foldertable) do
-  if #value ~= 1 then
-    file = file .. key .. '\t' .. table.concat(value,'\t') .. '\n'
-  end
-end
-
-output:write(file)
---]]-- end old code
 
 local input = io.open('inputfolders.txt')
 local output = io.open('folders.txt', 'w+')
